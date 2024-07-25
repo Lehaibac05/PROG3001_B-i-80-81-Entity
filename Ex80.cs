8183c4b3922c541eb4044f83dd37543ca64b30c0
Lehaibac05 <147247418+Lehaibac05@users.noreply.github.com>
2024-07-25 17:03:28 +0700

    Add files via upload

---
+++ Ex80.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Tạo và bắt đầu một luồng mới để chạy hàm ff
+        Thread thread = new Thread(new ThreadStart(ff));
+        thread.Start();
+
+        // Chờ đợi luồng kết thúc
+        thread.Join();
+
+        // Hiển thị thông báo khi luồng kết thúc
+        Console.WriteLine("Đã thực hiện xong");
+    }
+
+    static void ff()
+    {
+        Random random = new Random();
+
+        while (true)
+        {
+            int n = random.Next();
+            if (n % 2025 == 0)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine(n);
+                Thread.Sleep(100); // Delay 100ms
+            }
+        }
+    }
+}
